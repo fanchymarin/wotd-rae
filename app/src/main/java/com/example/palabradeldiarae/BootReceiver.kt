b984afcf1dd002aaeb5198a87d692fe8a2656336
Fran Marín Pérez <franmp97@outlook.com>
2025-03-12 19:48:47 +0100

    Log added to BootReceiver.kt

--- app/src/main/java/com/example/palabradeldiarae/BootReceiver.kt
+++ app/src/main/java/com/example/palabradeldiarae/BootReceiver.kt
@@ -3,13 +3,17 @@ package com.example.palabradeldiarae
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import com.example.palabradeldiarae.SetAlarm.Companion.setAlarm
+
+private val TAG: String = BootReceiver::class.java.getName()
 
 class BootReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action != "android.intent.action.BOOT_COMPLETED")
             return
+        Log.d(TAG, "Setting alarm on boot")
         setAlarm(context)
     }
 }
